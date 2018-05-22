47083a1c3ce2e074f64085a2342f467a3dd4059a
Joshua SUTTON <joshua.sutton@student.ecc.sa.edu.au>
2018-05-22 10:18:00 +0930

    final commit
    I realy should have done more commits along the way but was lazy

--- scoreBoard.java
+++ scoreBoard.java
@@ -4,12 +4,69 @@ import java.util.*;
 
 class scoreBoard{
   public static void main(String[] args){
-    person josh = new person("Josh", 6);
-    person oscar = new person("Oscar", 7);
-    josh.swap(oscar);
-    josh.list();
+    String fileString = "";
+    
+    
+    try{
+      Scanner sc = new Scanner(new File("scores.txt"));
+      while (sc.hasNextLine()){
+        fileString += sc.nextLine();
+      }
+      sc.close();
+    }catch(IOException e){
+      System.out.println(e);
+    }
+    String[] fileArr = fileString.split(" ");
+    person[] scores = new person[fileArr.length/2];
+    for(int i=0; i<scores.length; i++){
+      scores[i] = new person(fileArr[i*2], Double.parseDouble(fileArr[i*2+1]));
+    }
+    log(scores);
+    log(insertion(scores));
+    
   }
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  private static person[] insertion(person[] array){
+    person[] sortedArray = array.clone();
+    int count = 0;
+    
+    for(int i=1; i<sortedArray.length; i++){
+      int j = i;
+      while(j>0){
+        count++;
+        if(sortedArray[j-1].score<sortedArray[j].score){
+          count++;
+          person temp = sortedArray[j];
+          sortedArray[j] = sortedArray[j-1];
+          sortedArray[j-1] = temp;
+        }
+        j--;
+      }
+    }
+
+    return sortedArray;
+    
+  }
+  public static void log(person[] array){
+    for(int i=0; i<array.length; i++){
+      if(i>0){
+        System.out.print(", ");
+      }
+      System.out.print(array[i].list());
+    }
+    System.out.println();
+    
+  }
 }
 
 
@@ -21,8 +78,8 @@ class person{
     this.name = name;
     this.score = score;
   }
-  public void list(){
-   System.out.println(name+": "+score);
+  public String list(){
+    return name+": "+score;
     
   }
   public void swap(person other){
